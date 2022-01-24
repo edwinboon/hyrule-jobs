fff20831c6191a0cbf29c6a49f6f3ab87906311a
Edwin Boon <edwin.boon@eo.nl>
2022-01-24 12:04:51 +0100

    Add new component in combination with TS

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <template>
  <div class="app">
-   Hello Hyrule
+  <job-list :jobs="jobs" />
  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
+import  JobList from './components/JobList.vue'
+import { Job } from './types/Job'
 
 export default defineComponent({
   name: 'App',
-  components: {}
+  components: {
+    JobList
+  },
+  setup() {
+    const jobs = ref<Job[]>([
+      { title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1' },
+      { title: 'quarryman', location: 'death mountain', salary: 40000, id: '2' },
+      { title: 'flute player', location: 'the lost woods', salary: 35000, id: '3' },
+      { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
+      { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' },
+    ]);
+
+    return { jobs }
+  },
 });
 </script>
